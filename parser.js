335c9117caa4bc3310fe14ef3f94c566c56a9bf0
discountry <331027793@qq.com>
2016-08-24 05:05:16 +0000

    ip

--- parser.js
+++ parser.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get('/',function (req,res) {
-    var json = {"ipaddress":req.ip,"language":req.acceptsLanguages()[0],"software":req.headers['user-agent'].match(/[(][\w\ \;]*[)]/gi)[0]};
+    var json = {"ipaddress":req.headers['x-forwarded-for'] || req.connection.remoteAddress,"language":req.acceptsLanguages()[0],"software":req.headers['user-agent'].match(/[(][\w\ \;]*[)]/gi)[0]};
     res.end(JSON.stringify(json));
 });
 
